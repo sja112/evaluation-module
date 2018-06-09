c55147293aa9571b09f9a1c6ac272e3271c0eff1
Sandesh Jain <sanjain1@publicisgroupe.net>
2018-06-09 17:44:06 +0530

    Added comments and readme.txt file

---
+++ modules/site_information_advanced/readme.txt
@@ -0,0 +1,7 @@
+This module is used to alter the site configuration form. Module have dependency on serializer module.
+
+-- It will allow administrator user to add site api key
+
+-- It will provide JSON representation of any node. 
+
+-- Url to create JSON (/site_information_advanced/{site_api_key}/node/{node_id})
--- modules/site_information_advanced/src/Controller/NodeJson.php
+++ modules/site_information_advanced/src/Controller/NodeJson.php
@@ -6,18 +6,21 @@ use Drupal\node\Entity\Node;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
- *  Controller for page node to get Json.
+ *  Controller to get JSON representation of any node.
  */
 class NodeJson {
 
   /**
-   * {@inheritdoc}
+   * Display the node JSON.
    */
   public function nodeJson($site_api_key, $id) {
+    // Serializer service to encode data.
     $serializer = \Drupal::service('serializer');
+    // Get site api key from updated site information form.
     $siteapikey = \Drupal::config('system.site')->get('siteapikey');
     $node = Node::load($id);
     $json_data = NULL;
+    // Check site api key getting from url is same as entered in site information form.
     if ($siteapikey === $site_api_key) {
       $json_data = $serializer->serialize($node, 'json', ['plugin_id' => 'entity']);
       \Drupal::logger('site_information_advanced')->info('JSON is created for =>' . $id);
